99ffb7fff6af48e66edb16ba10fa9010bed75091
Moiz Ali <your-github-email@example.com>
2026-09-11 19:34:56 +0500

    Update Groish website

--- apps/api/src/main.js
+++ apps/api/src/main.js
@@ -16,7 +16,7 @@ import { verifyMailer } from './utils/mailer.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const distPath = path.resolve(__dirname, '../../dist');
+const distPath = path.resolve(__dirname, '../../web/dist');
 
 const app = express();
 
